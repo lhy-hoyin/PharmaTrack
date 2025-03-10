56aa086c8c9ee794d6d0afb819d7b7f65637870f
Ho Yin <88828097+lhy-hoyin@users.noreply.github.com>
2025-03-10 07:37:04 +0000

    Update UI of login and register components

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,14 +1,23 @@
 import React, { useState } from 'react';
+import {
+  FormLabel,
+  FormControl,
+  Input,
+  Button,
+} from '@chakra-ui/react'
 import authService from '../services/authService';
 
 const LoginForm = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [isSubmitting, setSubmitting] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
+      setSubmitting(true);
       await authService.login({ username, password });
+      setSubmitting(false);
       alert('Login successful');
     } catch (error) {
       alert('Login failed');
@@ -17,15 +26,21 @@ const LoginForm = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div>
-        <label>Username:</label>
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-      </div>
-      <button type="submit">Login</button>
+      <FormControl>
+        <FormLabel htmlFor='username'>Username</FormLabel>
+        <Input type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)} 
+          required />
+        <FormLabel htmlFor='password'>Password</FormLabel>
+        <Input type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)} 
+          required />
+      </FormControl>
+      <Button mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit'>
+        Login
+      </Button>
     </form>
   );
 };
--- frontend/src/components/RegisterForm.js
+++ frontend/src/components/RegisterForm.js
@@ -1,14 +1,23 @@
 import React, { useState } from 'react';
+import {
+  FormLabel,
+  FormControl,
+  Input,
+  Button,
+} from '@chakra-ui/react'
 import authService from '../services/authService';
 
 const RegisterForm = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [isSubmitting, setSubmitting] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
+      setSubmitting(true);
       await authService.register({ username, password });
+      setSubmitting(false);
       alert('Registration successful');
     } catch (error) {
       alert('Registration failed');
@@ -17,16 +26,22 @@ const RegisterForm = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div>
-        <label>Username:</label>
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-      </div>
-      <button type="submit">Register</button>
-    </form>
+          <FormControl>
+            <FormLabel htmlFor='username'>Username</FormLabel>
+            <Input type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)} 
+              required />
+            <FormLabel htmlFor='password'>Password</FormLabel>
+            <Input type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)} 
+              required />
+          </FormControl>
+          <Button mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit'>
+            Register
+          </Button>
+        </form>
   );
 };
 
