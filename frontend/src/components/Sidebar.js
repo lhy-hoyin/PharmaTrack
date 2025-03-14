ed255b93e289ceff9cc6db5acacc458cc11a5418
Ho Yin <88828097+lhy-hoyin@users.noreply.github.com>
2025-03-14 10:18:01 +0000

    Implement dashboard barebones with sidebar for selection of content to display

---
+++ frontend/src/components/Sidebar.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { UnorderedList, ListItem, Divider, Link } from '@chakra-ui/react';
+
+
+const Sidebar = ({selectContent}) => {
+    const [] = useState('procurement');
+
+    return (
+        <div>
+        <h2>Procurement</h2>
+        <UnorderedList>
+          <ListItem onClick={() => selectContent('create-order')}>
+            <Link>Create New Order</Link>
+          </ListItem>
+          <ListItem onClick={() => selectContent('approve-orders')}>
+            <Link>Approve Orders</Link>
+          </ListItem>
+          <ListItem onClick={() => selectContent('view-orders')}>
+            <Link>View Orders</Link>
+          </ListItem>
+        </UnorderedList>
+        <br />
+
+        <h2>Stock Monitor</h2>
+        <UnorderedList>
+          <ListItem onClick={() => selectContent('stock-monitor')}>
+            <Link>View Stock</Link>
+          </ListItem>
+          <ListItem onClick={() => selectContent('stock-history')}>
+            <Link>History Log</Link>
+          </ListItem>
+        </UnorderedList>
+        <br />
+
+        <h2>Prescriptions</h2>
+        <UnorderedList>
+          <ListItem onClick={() => selectContent('view-prescription')}>
+            <Link>View Stock</Link>
+          </ListItem>
+          <ListItem onClick={() => selectContent('prescription-history')}>
+          <Link>History Log</Link>
+          </ListItem>
+        </UnorderedList>
+      </div>
+    );
+};
+
+export default Sidebar;
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from '@chakra-ui/react';
 import { useAuth } from '../context/AuthContext';
 import authService from '../services/authService';
+import Sidebar from '../components/Sidebar.js';
+import "./Dashboard.css";
 
-const DashboardPage = () => {
+const Dashboard = () => {
   const { logout } = useAuth();
+  const [content, displayContentOf] = useState('stock-monitor');
+  
+  const renderContent = () => {
+    switch (content) {
+      case 'create-order':
+        return <div>Create order</div>;
+      case 'approve-orders':  
+        return <div>Approve orders</div>;
+      case 'view-orders':
+        return <div>View orders</div>;
+
+      case 'stock-monitor':
+        return <div>Stock Monitor</div>;
+      case 'stock-history':
+        return <div>Stock History Log</div>;
+
+      case 'view-prescription':
+        return <div>View Prescription</div>;  
+      case 'prescription-history':
+        return <div>Prescriptions History Log</div>;
+
+      default: return <div />;
+    }
+  };
 
   return (
-    <div>
-      <h2>Dashboard</h2>
-      <button onClick={logout}>Logout</button>
+    <div className="dashboard-container">
+      <div className="dashboard-header">
+        <a href="/dashhboard"><img src="FIXME.png" /></a>
+        <Button onClick={logout} colorScheme="teal">Logout</Button>
+      </div>
+      <div className="dashboard-content">
+        <div className="dashboard-sidebar">
+        <Sidebar selectContent={displayContentOf} />
+        </div>
+        <div className="dashboard-main">
+          {renderContent()}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default DashboardPage;
+export default Dashboard;
